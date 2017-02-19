7c0e60f76a22c455e0e974233a9ed01225a3720f
Syed Ahmed <geeklycan@gmail.com>
2017-02-19 17:55:42 +0600

    slider js & style added

--- wp-responsive-slides.php
+++ wp-responsive-slides.php
@@ -14,17 +14,50 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 class WP_Responsive_Slides{
   public function __construct(){
     // plugin activation code
+    register_activation_hook(__FILE__, array($this, 'wp_res_sld_activation'));
 
     // plugin deactivation code
+    register_deactivation_hook(__FILE__, array($this, 'wp_res_sld_deactivation'));
 
     // plugin admin enqueue scripts & style
 
-    // plugin front end enqueue scripts & style
+    // plugin front end enqueue styles
+    add_action('wp_enqueue_scripts', array($this, 'wp_res_sld_front_styles'));
+
+    // plugin front end enqueue scripts
+    add_action('wp_enqueue_scripts', array($this, 'wp_res_sld_front_scripts'));
 
     // plugin shortcode initialize
 
 
   }
+
+  // slider activation code goes here
+  public function wp_res_sld_activation(){
+
+  }
+
+  // slider deactivation code goes here
+  public function wp_res_sld_deactivation(){
+
+  }
+
+  // slider front scripts
+  public function wp_res_sld_front_scripts(){
+    wp_enqueue_script('jquery');
+    // responsive slide core JS inlcude
+    wp_register_script('responsiveslides_core', plugins_url('js/responsiveslides.min.js', __FILE__),array("jquery"));
+    wp_enqueue_script('responsiveslides_core');
+    // responsive slide initialize JS inlcude
+    wp_register_script('responsiveslides_init', plugins_url('js/responsiveslides.init.js', __FILE__));
+    wp_enqueue_script('responsiveslides_init');
+  }
+
+  public function wp_res_sld_front_styles(){
+    wp_register_style('responsiveslides_style', plugins_url('css/responsiveslides.css', __FILE__));
+    wp_enqueue_style('responsiveslides_style');
+  }
+
 }
 
 global $wp_res_slides;
